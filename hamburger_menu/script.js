58a259ddf86f82eb68bd63b21e2f962fdb5c238f
허윤우 <hyoon516@naver.com>
2023-01-07 12:04:39 +0900

    js를 활용한 움직이는 햄버거메뉴를 구현해봄.

---
+++ hamburger_menu/script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn')
+const nav = document.getElementById('nav')
+
+btn.addEventListener("click", function () {
+        nav.classList.toggle('active')
+        btn.classList.toggle('active')
+    })
